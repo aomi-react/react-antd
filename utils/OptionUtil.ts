dd24d9aae2770977ed7f7862b4fc49600575f4a6
尼古拉斯丶少 <sean.snow@live.com>
2021-06-20 14:37:40 +0800

    fix: 当数据不存在时，有默认值修复

--- utils/OptionUtil.ts
+++ utils/OptionUtil.ts
@@ -42,7 +42,7 @@ export function entity2Options<T>(entities: T | Array<T>, options: Options<T> = 
         value: options.getValue ? options.getValue(item, index) : (item.id || item.code || item.name || item.value || index)
       });
     });
-  } else {
+  } else if (!!entities) {
     const tmp: any = entities;
     result.push({
       label: options.getLabel ? options.getLabel(tmp, 0) : (tmp.name || tmp.label || tmp.code || 0),
