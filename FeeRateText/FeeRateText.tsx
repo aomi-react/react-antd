1b775e0768f383b995ed43dd4c602b7a5578dab4
尼古拉斯丶少 <sean.snow@live.com>
2021-07-01 16:12:00 +0800

    feat: 传递参数

--- FeeRateText/FeeRateText.tsx
+++ FeeRateText/FeeRateText.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
-import { Descriptions } from 'antd';
+import { Descriptions, DescriptionsProps } from 'antd';
 import { FeeRate, feeRateType } from '@aomi/common-service/FeeRate/FeeRate';
 
 /**
  * 费率展示
  */
-export function FeeRateText({ type, value, min, max }: FeeRate) {
+export function FeeRateText({ type, value, min, max, ...props }: FeeRate & DescriptionsProps) {
   const unit = type === feeRateType.cap ? '元' : '%';
   return (
-    <Descriptions size="small" column={1}>
+    <Descriptions size="small" column={1} {...props}>
       <Descriptions.Item label="每笔收费">{`${value}${unit}`}</Descriptions.Item>
       {type !== feeRateType.cap && (
         <Descriptions.Item label={'每笔最低收费'}>{`${min}元`}</Descriptions.Item>
